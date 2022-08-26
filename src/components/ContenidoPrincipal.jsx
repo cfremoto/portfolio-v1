90b7a6c2f157a489ef28df46b286cea895a425d6
Ing. Cesar Fernandez <cesar.bfernandez@gmail.com>
2022-08-25 21:32:03 -0400

    modo oscuro y correcciones menores2

--- src/components/ContenidoPrincipal.jsx
+++ src/components/ContenidoPrincipal.jsx
@@ -5,10 +5,10 @@ const ContenidoPrincipal = () => {
   return (
     <section className="home">
       <div className="content">
-        <span className="hi">Hi There...</span>
-        <h3>I am <span> César A. Fernández</span>  </h3>
-        <p className="info" > I am a Full-Stack Developer </p>
-        <p className="text"> Soy Full Stack Developer, mi objetivo es incorporarme a un equipo de desarrollo para aportar mis conocimientos y habilidades como pensamiento creativo, liderazgo, resolución de problemas, trabajo en equipo y comunicación, las cuales desarrollé gracias a mi experiencia previa de gerente. </p>
+        <span className="hi">Hola..!!!.</span>
+        <h3>Soy <span> César A. Fernández</span>  </h3>
+        <p className="info" > Soy un desarrolador Full-Stack </p>
+        <p className="text"> mi objetivo es incorporarme a un equipo de desarrollo para aportar mis conocimientos y habilidades como pensamiento creativo, liderazgo, resolución de problemas, trabajo en equipo y comunicación, las cuales desarrollé gracias a mi experiencia previa de gerente. </p>
         <Link to="/about" className="btn">sobre mi</Link>
       </div>
 
